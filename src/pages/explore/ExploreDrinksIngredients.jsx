68ec1b9d674cdbbd5d3be8d902f066dc3b0cf58d
Vitor Emerich <vitoremerich.contato@gmail.com>
2022-03-10 18:21:36 -0300

    Em andamento Tela de Ingredientes

--- src/pages/explore/ExploreDrinksIngredients.jsx
+++ src/pages/explore/ExploreDrinksIngredients.jsx
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { exploreDrinkIngredients }
+from '../../services/exploreIngredientsAPI';
 
 export default class ExploreDrinksIngredients extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ingredients: [],
+      ingredientsCards: [],
+    };
+    this.mapIngredientes = this.mapIngredientes.bind(this);
+  }
+
+  componentDidMount() {
+    this.getDrinkIngredients();
+  }
+
+  getDrinkIngredients = async () => {
+    const fetchIngredients = await exploreDrinkIngredients();
+    this.setState({
+      ingredients: fetchIngredients,
+    }, () => this.mapIngredientes());
+  }
+
+  mapIngredientes() {
+    const { ingredients } = this.state;
+    const ingredientsArray = [];
+    const MAX_NUMBER = 12;
+    const teste = ingredients.map(({ strIngredient1 }) => strIngredient1);
+    for (let i = 1; i <= MAX_NUMBER; i += 1) {
+      ingredientsArray.push(teste[i]);
+    }
+    this.setState({
+      ingredientsCards: ingredientsArray,
+    });
+  }
+
   render() {
     const { history } = this.props;
+    const { ingredientsCards } = this.state;
     return (
       <div>
         <Header history={ history } name="Explore Ingredients" hideSearch />
+        {ingredientsCards.map((card) => (
+          <Link to="/drinks" key={ card }>
+            <div>
+              <img
+                src={ `https://www.thecocktaildb.com/images/ingredients/${card}.png` }
+                alt={ card }
+              />
+              <h1>{card}</h1>
+            </div>
+          </Link>
+        ))}
         <Footer />
       </div>
     );
--- src/pages/explore/ExploreFoodsIngredients.jsx
+++ src/pages/explore/ExploreFoodsIngredients.jsx
@@ -2,13 +2,59 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { exploreFoodIngredients }
+from '../../services/exploreIngredientsAPI';
 
 export default class ExploreFoodsIngredients extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ingredients: [],
+      ingredientsCards: [],
+    };
+    this.mapIngredientes = this.mapIngredientes.bind(this);
+  }
+
+  componentDidMount() {
+    this.getFoodIngredients();
+  }
+
+  getFoodIngredients = async () => {
+    const fetchIngredients = await exploreFoodIngredients();
+    this.setState({
+      ingredients: fetchIngredients,
+    }, () => this.mapIngredientes());
+  }
+
+  mapIngredientes() {
+    const { ingredients } = this.state;
+    const ingredientsArray = [];
+    const MAX_NUMBER = 12;
+    const teste = ingredients.map(({ strIngredient }) => strIngredient);
+    for (let i = 1; i <= MAX_NUMBER; i += 1) {
+      ingredientsArray.push(teste[i]);
+    }
+    this.setState({
+      ingredientsCards: ingredientsArray,
+    });
+  }
+
   render() {
     const { history } = this.props;
+    const { ingredientsCards } = this.state;
     return (
       <div>
         <Header history={ history } name="Explore Ingredients" hideSearch />
+        {ingredientsCards.map((card) => (
+          <div key={ card }>
+            <img
+              src={ `https://www.themealdb.com/images/ingredients/${card}-Small.png` }
+              alt={ card }
+            />
+            <h1>{card}</h1>
+          </div>
+        ))}
         <Footer />
       </div>
     );
